b89273cd0f6a89e5e11bdd4a2e5e729942e1f43a
CarlBateman <CarlBateman@hotmailcom>
2024-07-11 12:31:46 +0100

    Made free variables private with module pattern

--- src/user-interaction.js
+++ src/user-interaction.js
@@ -10,164 +10,167 @@ const Interaction = {
   Transform: "Transform",
 };
 
-let interaction = Interaction.None;
+function MakeUserInteraction() {
+  let interaction = Interaction.None;
 
-let pointer = new THREE.Vector2();
+  let pointer = new THREE.Vector2();
 
-let shiftKey = false;
-let ctrlKey = false;
-let altKey = false;
+  let shiftKey = false;
+  let ctrlKey = false;
+  let altKey = false;
 
-let currentSelection, grabPoint;
+  let currentSelection, grabPoint;
 
-let dragging = false;
-let mouseDown = false;
+  let dragging = false;
+  let mouseDown = false;
 
-let plane = new THREE.Plane();
-let control, orbit;
+  let plane = new THREE.Plane();
+  let control, orbit;
 
-const userinteraction = {
-  setup: function () {
-    orbit = new OrbitControls(camera, renderer.domElement);
+  const userinteraction = {
+    setup: function () {
+      orbit = new OrbitControls(camera, renderer.domElement);
 
-    control = new TransformControls(camera, renderer.domElement);
-    control.setMode("scale");
-    control.enabled = false;
-    scene.add(control);
-  },
-
-  onKeyDown: function (e) {
-    shiftKey = e.shiftKey;
-    ctrlKey = e.ctrlKey;
-    altKey = e.altKey;
-  },
-
-  onKeyUp: function (e) {
-    shiftKey = e.shiftKey;
-    ctrlKey = e.ctrlKey;
-    altKey = e.altKey;
-    // if (e.key == "Delete" || e.key == "Backspace") {
-    //   if (currentSelection != null) {
-    //     control.detach();
-
-    //     const index = meshes.indexOf(currentSelection);
-    //     meshes.splice(index, 1);
-    //     inactiveMeshes.push(currentSelection);
-
-    //     currentSelection.visible = false;
-    //     currentSelection = null;
-    //   }
-    //   return;
-    // }
-  },
-
-  onMouseUp: function (event) {
-    if (orbit.enabled && !dragging) {
-      currentSelection = null;
-      control.detach();
+      control = new TransformControls(camera, renderer.domElement);
+      control.setMode("scale");
       control.enabled = false;
-    }
-    dragging = false;
-    interaction = Interaction.None;
-    orbit.enabled = true;
-    mouseDown = false;
-  },
+      scene.add(control);
+    },
 
-  onMouseDown: function (event) {
-    const raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera(pointer, camera);
-    mouseDown = true;
+    onKeyDown: function (e) {
+      shiftKey = e.shiftKey;
+      ctrlKey = e.ctrlKey;
+      altKey = e.altKey;
+    },
 
-    const intersects = raycaster.intersectObjects(meshes);
-    if (intersects.length > 0) {
-      if (interaction !== Interaction.Transform) {
-        orbit.enabled = false;
-        interaction = Interaction.Drag;
+    onKeyUp: function (e) {
+      shiftKey = e.shiftKey;
+      ctrlKey = e.ctrlKey;
+      altKey = e.altKey;
+      // if (e.key == "Delete" || e.key == "Backspace") {
+      //   if (currentSelection != null) {
+      //     control.detach();
 
-        currentSelection = intersects[0].object;
-        // outlinePass.selectedObjects = [currentSelection];
+      //     const index = meshes.indexOf(currentSelection);
+      //     meshes.splice(index, 1);
+      //     inactiveMeshes.push(currentSelection);
 
-        const fwd = new THREE.Vector3();
-        camera.getWorldDirection(fwd);
+      //     currentSelection.visible = false;
+      //     currentSelection = null;
+      //   }
+      //   return;
+      // }
+    },
 
-        const d = currentSelection.position
-          .clone()
-          .negate()
-          .projectOnVector(fwd);
-        const sd = d.dot(fwd);
-        plane.set(fwd, sd);
-
-        const intersect = new THREE.Vector3();
-        raycaster.ray.intersectPlane(plane, intersect);
-        grabPoint = intersect;
-      }
-    } else {
-      orbit.enabled = true;
-      interaction = Interaction.Orbit;
-    }
-  },
-
-  onDoubleClick: function () {
-    if (currentSelection) {
-      if (control.enabled && control.mode === "scale") {
-        interaction = Interaction.None;
-        control.setMode("scale");
+    onMouseUp: function (event) {
+      if (orbit.enabled && !dragging) {
+        currentSelection = null;
         control.detach();
         control.enabled = false;
-      } else {
-        interaction = Interaction.Transform;
-        control.attach(currentSelection);
-        switch (control.mode) {
-          case "translate":
-            control.setMode("rotate");
-            break;
-          case "rotate":
-            control.setMode("scale");
-            break;
-          case "scale":
-            control.setMode("translate");
-            break;
-        }
-        control.enabled = true;
-        orbit.enabled = false;
       }
-    }
-  },
+      dragging = false;
+      interaction = Interaction.None;
+      orbit.enabled = true;
+      mouseDown = false;
+    },
 
-  onPointerMove: function (event) {
-    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    dragging = mouseDown;
-
-    if (interaction === Interaction.Drag) {
-      const newGrabPoint = new THREE.Vector3();
+    onMouseDown: function (event) {
       const raycaster = new THREE.Raycaster();
       raycaster.setFromCamera(pointer, camera);
-      raycaster.ray.intersectPlane(plane, newGrabPoint);
+      mouseDown = true;
 
-      // let change = grabPoint.clone();
-      // change.sub(newGrabPoint);
+      const intersects = raycaster.intersectObjects(meshes);
+      if (intersects.length > 0) {
+        if (interaction !== Interaction.Transform) {
+          orbit.enabled = false;
+          interaction = Interaction.Drag;
 
-      const change = grabPoint.clone().sub(newGrabPoint);
+          currentSelection = intersects[0].object;
 
-      if (ctrlKey) {
-        const a = grabPoint.clone();
-        const b = newGrabPoint.clone();
-        currentSelection.worldToLocal(a);
-        currentSelection.worldToLocal(b);
-        const ab = a.clone().cross(b);
-        const r = b.angleTo(a);
-        currentSelection.rotateZ(r * Math.sign(ab.z));
+          const fwd = new THREE.Vector3();
+          camera.getWorldDirection(fwd);
+
+          const d = currentSelection.position
+            .clone()
+            .negate()
+            .projectOnVector(fwd);
+          const sd = d.dot(fwd);
+          plane.set(fwd, sd);
+
+          const intersect = new THREE.Vector3();
+          raycaster.ray.intersectPlane(plane, intersect);
+          grabPoint = intersect;
+        }
       } else {
-        currentSelection.position.sub(change);
+        orbit.enabled = true;
+        interaction = Interaction.Orbit;
       }
-      grabPoint = newGrabPoint;
-    }
-  },
+    },
 
-  getCurrentSelection: function () {
-    return currentSelection;
-  },
-};
+    onDoubleClick: function () {
+      if (currentSelection) {
+        if (control.enabled && control.mode === "scale") {
+          interaction = Interaction.None;
+          control.setMode("scale");
+          control.detach();
+          control.enabled = false;
+        } else {
+          interaction = Interaction.Transform;
+          control.attach(currentSelection);
+          switch (control.mode) {
+            case "translate":
+              control.setMode("rotate");
+              break;
+            case "rotate":
+              control.setMode("scale");
+              break;
+            case "scale":
+              control.setMode("translate");
+              break;
+          }
+          control.enabled = true;
+          orbit.enabled = false;
+        }
+      }
+    },
+
+    onPointerMove: function (event) {
+      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+      pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+      dragging = mouseDown;
+
+      if (interaction === Interaction.Drag) {
+        const newGrabPoint = new THREE.Vector3();
+        const raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera(pointer, camera);
+        raycaster.ray.intersectPlane(plane, newGrabPoint);
+
+        // let change = grabPoint.clone();
+        // change.sub(newGrabPoint);
+
+        const change = grabPoint.clone().sub(newGrabPoint);
+
+        if (ctrlKey) {
+          const a = grabPoint.clone();
+          const b = newGrabPoint.clone();
+          currentSelection.worldToLocal(a);
+          currentSelection.worldToLocal(b);
+          const ab = a.clone().cross(b);
+          const r = b.angleTo(a);
+          currentSelection.rotateZ(r * Math.sign(ab.z));
+        } else {
+          currentSelection.position.sub(change);
+        }
+        grabPoint = newGrabPoint;
+      }
+    },
+
+    getCurrentSelection: function () {
+      return currentSelection;
+    },
+  };
+  return userinteraction;
+}
+let userinteraction = MakeUserInteraction();
 
 export { userinteraction };
