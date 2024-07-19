8dd80828848b2c2bf4aec09f9879079cf9bf764b
CarlBateman <CarlBateman@hotmailcom>
2024-07-19 07:16:57 +0100

    DatGui experiment

--- src/controller.js
+++ src/controller.js
@@ -11,6 +11,9 @@ let camera, scene, renderer, composer, effectFXAA, outlinePass;
 let meshes = [];
 let curr;
 
+const imgui = ImGui.default();
+const ImGui_Impl = ImGui_Impl_Three.default();
+
 function setupCamera() {
   camera = new THREE.PerspectiveCamera(
     70,
@@ -54,6 +57,12 @@ function setupRenderer() {
   renderer.setAnimationLoop(animation);
   renderer.domElement.id = "editor-view";
   document.body.appendChild(renderer.domElement);
+
+  // Initialize ImGui
+  const ctx = renderer.getContext();
+  const canvas = renderer.domElement;
+  imgui.CreateContext();
+  ImGui_Impl.Init(canvas);
 }
 
 function setupPostProcessing() {
@@ -143,136 +152,6 @@ function debugShowPoint(pos) {
   const mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(...pos);
   scene.add(mesh);
-}
-
-const infoDiv = document.getElementById("info");
-
-function getMeshProperties(mesh) {
-  return {
-    position: mesh.position,
-    rotation: mesh.rotation,
-    scale: mesh.scale,
-    materialColor: mesh.material.color,
-    geometryType: mesh.geometry.type,
-  };
-}
-
-function createPropertyEventHandler(object, property, axis) {
-  return function (event) {
-    object[property][axis] = parseFloat(event.target.value);
-  };
-}
-
-function createColorEventHandler(material) {
-  return function (event) {
-    material.color.set(event.target.value);
-  };
-}
-
-function setEventHandlers() {
-  document.querySelectorAll("input[data-prop]").forEach((input) => {
-    const [property, axis] = input.getAttribute("data-prop").split("-");
-    if (property === "color") {
-      input.addEventListener(
-        "input",
-        createColorEventHandler(userinteraction.getCurrentSelection().material)
-      );
-    } else {
-      input.addEventListener(
-        "input",
-        createPropertyEventHandler(
-          userinteraction.getCurrentSelection(),
-          property,
-          axis
-        )
-      );
-    }
-  });
-}
-
-function generateTableRow(propertyName, property, labels) {
-  let row = `<tr><td class="propertyName">${propertyName}</td>`;
-  labels.forEach((label) => {
-    const axis = label.toLowerCase();
-    const value = property[axis] !== undefined ? property[axis].toFixed(2) : 0;
-    row += `<td><label for="${propertyName}-${axis}">${label}: </label><input type="number" id="${propertyName}-${axis}" data-prop="${propertyName}-${axis}" value="${value}"></td>`;
-  });
-  row += "</tr>";
-  return row;
-}
-
-function updateInputValue(propertyName, property, labels) {
-  labels.forEach((label) => {
-    const axis = label.toLowerCase();
-    const input = document.getElementById(`${propertyName}-${axis}`);
-    if (input) {
-      input.value =
-        property[axis] !== undefined ? property[axis].toFixed(2) : 0;
-    }
-  });
-}
-
-function updateColorInputValue(color) {
-  const input = document.getElementById("color");
-  if (input) {
-    input.value = `#${color.getHexString()}`;
-  }
-}
-
-// Function to initialize the HTML div with mesh properties in a table
-function initializeInfoDiv(properties) {
-  let tableContent = "";
-
-  if (properties.position) {
-    // prettier-ignore
-    tableContent += generateTableRow('position', properties.position, ['X', 'Y', 'Z']);
-  }
-  if (properties.rotation) {
-    // prettier-ignore
-    tableContent += generateTableRow('rotation', properties.rotation, ['X', 'Y', 'Z']);
-  }
-  if (properties.scale) {
-    // prettier-ignore
-    tableContent += generateTableRow('scale', properties.scale, ['X', 'Y', 'Z']);
-  }
-  if (properties.materialColor) {
-    tableContent += `
-                <tr>
-                    <td>Material Color</td>
-                    <td colspan="3"><label for="color">Color: </label><input type="color" id="color" data-prop="color" value="#${properties.materialColor.getHexString()}"></td>
-                </tr>
-            `;
-  }
-  if (properties.geometryType) {
-    tableContent += `
-                <tr>
-                    <td>Geometry Type</td>
-                    <td colspan="3">${properties.geometryType}</td>
-                </tr>
-            `;
-  }
-
-  infoDiv.innerHTML = `<strong>Properties</strong><table class="blueTable">${tableContent}</table>`;
-
-  // Set the event handlers after updating the HTML
-  setEventHandlers();
-}
-
-// Function to update the HTML div with mesh properties in a table
-function updateInfoDiv(properties) {
-  if (properties.position) {
-    // prettier-ignore
-    updateInputValue('position', properties.position, ['X', 'Y', 'Z']);
-  }
-  if (properties.rotation) {
-    updateInputValue("rotation", properties.rotation, ["X", "Y", "Z"]);
-  }
-  if (properties.scale) {
-    updateInputValue("scale", properties.scale, ["X", "Y", "Z"]);
-  }
-  if (properties.materialColor) {
-    updateColorInputValue(properties.materialColor);
-  }
 }
 
 const controller = {
